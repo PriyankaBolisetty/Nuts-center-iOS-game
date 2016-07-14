58fe65a8cec067139ba8e1a12956d56b71221df4
PriyankaBolisetty <PriyankaBolisetty@users.noreply.github.com>
2016-07-14 13:07:20 -0700

    Update nutscontrolcenterViewController.swift

--- Nuts Center/nutscontrolcenterViewController.swift
+++ Nuts Center/nutscontrolcenterViewController.swift
@@ -2,8 +2,8 @@
 //  nutscontrolcenterViewController.swift
 //  Nuts Center
 //
-//  Created by lena on 3/28/16.
-//  Copyright © 2016 lena. All rights reserved.
+//  Created by Priyanka
+//  Copyright © 2016 Priyanka. All rights reserved.
 //
 
 import UIKit
